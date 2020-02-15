5ac172d5e8828838233f845b156e54420521de4d
yfarhod <49104417+yfarhod@users.noreply.github.com>
2020-02-15 13:15:30 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Routes from "routes";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return (
+    <div>
+      <h3> Welcome to hooks </h3>{" "}
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
